91c6cdcf2359355fd32c7065231f35be17e558c4
Augusto Manoel <78373460+GuxtoOo@users.noreply.github.com>
2021-09-15 13:25:47 -0300

    Add files via upload

---
+++ MinhaApi/MinhaApi.Domain/Repositories/IUsersRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MinhaApi.Repository
+{
+    public interface IUsersRepository
+    {
+        Task<IEnumerable<dynamic>> GetAsync();
+        Task<dynamic> GetByIdAsync(long userId);
+
+        Task AddAsync(dynamic user);
+        Task DeleteAsync(long userId);
+        Task UpdateAsync(long userId, dynamic user);
+    }
+}
---
+++ MinhaApi/MinhaApi.Domain/Services/IUsersService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaApi.Domain.Services
+{
+    public interface IUsersService
+    {
+        Task<IEnumerable<dynamic>> GetAsync();
+        Task<dynamic> GetByIdAsync(long userId);
+
+        Task AddAsync(dynamic user);
+        Task DeleteAsync(long userId);
+        Task UpdateAsync(long userId, dynamic user);
+    }
+}
