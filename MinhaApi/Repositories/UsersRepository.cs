adb877113d7b10095461b5877fbd51a276f53e2f
Augusto Manoel <78373460+GuxtoOo@users.noreply.github.com>
2021-09-15 13:31:00 -0300

    Add files via upload

---
+++ MinhaApi/Repositories/UsersRepository.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MinhaApi.Repository
+{
+    public class UsersRepository : IUsersRepository
+    {
+        public UsersRepository()
+        {
+        }
+
+        public Task AddAsync(dynamic user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(long userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<dynamic>> GetAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<dynamic> GetByIdAsync(long userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<dynamic> GetUsers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public dynamic GetUsersById(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(dynamic user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(long userId, dynamic user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ MinhaApi/Services/UsersService.cs
@@ -0,0 +1,47 @@
+﻿using MinhaApi.Domain.Services;
+using MinhaApi.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaApi.Infrastructure.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly IUsersRepository _usersRepository;
+
+        public UsersService(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        public async Task AddAsync(dynamic user)
+        {
+           await _usersRepository.AddAsync(user);
+        }
+
+        public async Task DeleteAsync(long userId)
+        {
+            await _usersRepository.DeleteAsync(userId);
+        }
+
+        public async Task<IEnumerable<dynamic>> GetAsync()
+        {
+            await _usersRepository.GetAsync();
+            return null;
+        }
+
+        public async Task<dynamic> GetByIdAsync(long userId)
+        {
+            await _usersRepository.GetByIdAsync(userId);
+            return null;
+        }
+
+        public async Task UpdateAsync(long userId, dynamic user)
+        {
+            await _usersRepository.UpdateAsync(userId, user);
+        }
+    }
+}
