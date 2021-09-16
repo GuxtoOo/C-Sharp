6bece870b4bac86055af97ad95a7422ac9405ce2
Augusto Manoel <78373460+GuxtoOo@users.noreply.github.com>
2021-09-16 12:42:01 -0300

    Add files via upload

---
+++ MinhaApi/MinhaApi.Infrastructure/Data/ApiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaApi.Domain.AggregatesModel;
+using MinhaApi.Domain.Seedwork;
+
+namespace MinhaApi.Infrastructure.Data
+{
+    public class ApiDbContext : DbContext, IUnitOfWork
+    {
+        public DbSet<User> Users { get; set; }
+       
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApiDbContext).Assembly);
+        }
+    }
+}
---
+++ MinhaApi/MinhaApi.Infrastructure/Data/EntityConfigurations/UsersEntityConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MinhaApi.Domain.AggregatesModel;
+
+namespace MinhaApi.Infrastructure.Data.EntityConfigurations
+{
+    public class UsersEntityConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("TB_USERS");
+
+            builder.Property(s => s.Id)
+                .ValueGeneratedOnAdd()
+            .HasColumnName("USU_ID_USER");
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Name)
+                .HasColumnName("USU_TX_NAME")
+                .HasColumnType("varchar")
+                .HasMaxLength(60)
+                .IsRequired();
+
+            builder.Property(s => s.Document)
+                .HasColumnName("USU_TX_DOCUMENT")
+                .HasColumnType("varchar")
+                .HasMaxLength(18)
+                .IsRequired();
+
+        }
+    }
+}
