11dba4981d07df13757a9ebce3d61fca3ade3236
Augusto Manoel <78373460+GuxtoOo@users.noreply.github.com>
2021-09-17 11:54:48 -0300

    Add files via upload

---
+++ MinhaApi/MinhaApi.Infrastructure/Repositories/UsersRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaApi.Domain.AggregatesModel;
+using MinhaApi.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MinhaApi.Repository
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly ApiDbContext _apiDbContext;
+
+        public UsersRepository(ApiDbContext apiDbContext)
+        {
+            _apiDbContext = apiDbContext;
+        }
+
+        public async Task<long> AddAsync(User user)
+        {
+
+            var command = await _apiDbContext.Users.AddAsync(user);
+
+            await _apiDbContext.SaveChangesAsync();
+
+            return command.Entity.Id;
+        }
+                
+        public async Task DeleteAsync(long id)
+        {
+            _apiDbContext.Users.Remove(_apiDbContext.Users.Find(id));
+
+            await _apiDbContext.SaveChangesAsync();
+        }
+
+        public Task<IEnumerable<dynamic>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> GetByIdAsync(long userId)
+        {
+            var query = await _apiDbContext.Users
+                .FirstOrDefaultAsync(s => s.Id == userId);
+
+            return query;
+        }
+
+        public async Task UpdateAsync(long userId, dynamic users)
+        {
+            var user = await GetByIdAsync(userId);
+
+            user.SetName("teste");
+
+            _apiDbContext.SaveChanges();
+        }
+    }
+}
